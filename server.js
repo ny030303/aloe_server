cc76847c56494b516ec1c7c6395de3515ceb60cf
jny <ny030303@naver.com>
2021-04-12 15:44:27 +0900

    add 0412

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const app = require('./app');
 const http = require('http');  //노드의 기본 모듈
-const socket = require('socket.io');
-
 const server = http.createServer(app);
-const io = socket(server);
+const io  = require('socket.io')(server, {
+    cors: {
+    //   origin: "http://localhost:3000",
+        origin: true,
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
+});
+// const io = socket(server);
 
 let userList = []; //로그인한 유저들을 저장하는 배열
 io.on("connect", socket => {
     console.log(socket.id + "연결");
 
-    // socket.on('login', data => {
-    //     userList.push({id:socket.id, nickname:data});
-    //     socket.emit('login-ok', {id:socket.id, nickname:data});
-    //     io.emit('user-list', userList);
-    // });
+    socket.on('login', data => {
+        userList.push({socket_id:socket.id, nickname:data});
+        socket.emit('login-ok', {id:socket.id, nickname:data});
+        io.emit('user-list', userList);
+    });
 
     // socket.on('disconnect', ()=>{
     //     let idx = userList.findIndex(x => x.id === socket.id);
